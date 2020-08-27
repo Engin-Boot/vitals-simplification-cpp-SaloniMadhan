4639549bea2b2041d148544e0c8bdd4866a2188a
SaloniMadhan <69685422+SaloniMadhan@users.noreply.github.com>
2020-08-27 12:11:03 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -1,6 +1,6 @@
 #include <assert.h>
 
-bool check_bpm(float bpm)
+/*bool check_bpm(float bpm)
 {
   return !(bpm < 70 || bpm > 150);
 }
@@ -11,9 +11,14 @@ bool check_spo2(float spo2)
 bool check_respRate(float respRate)
 {
  return !(respRate < 30 || respRate > 95);
+}*/
+
+bool isInsideRange(float value,float lowerLimit,float upperLimit)
+{
+  return (value < lowerLimit && value > upperLimit)
 }
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  return (check_bpm(bpm) && check_spo2(spo2) && check_respRate(respRate)); 
+  return isInsideRange(bpm,70,150) && isInsideRange(spo2,90,100) &&isInsideRange(respRate,30,95);
 }
   /*if(bpm < 70 || bpm > 150) {
     return false;
@@ -28,4 +33,8 @@ bool vitalsAreOk(float bpm, float spo2, float respRate) {
 int main() {
   assert(vitalsAreOk(80, 95, 60) == true);
   assert(vitalsAreOk(60, 90, 40) == false);
+  assert(vitalsAreOk(160, 110, 100) == false); //all our above than every range
+  assert(vitalsAreOk(60, 85, 20) == false); // all are less than every range
+  assert(vitalsAreOk(80, 95, 20) == false); //two are in range
+  assert(vitalsAreOk(80, 50, 100) == false);  // one is in range
 }
